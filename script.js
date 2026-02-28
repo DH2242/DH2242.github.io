54c6bad51c35951330c630ce388ed051c12cf96a
Destin Huang <destinhuang3@gmail.com>
2026-02-28 18:11:14 +0000

    Tried to make the webcam turn on

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 let model;
+let webcamElement = document.getElementById('webcam');
+
+async function setupWebcam() {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false
+  });
+  webcamElement.srcObject = stream;
+
+  return new Promise((resolve) => {
+    webcamElement.onloadedmetadata = () => {
+      resolve();
+    };
+  });
+}
 
 async function loadModel() {
   model = await tf.loadLayersModel('tfjs_model/model.json');
   console.log("Model loaded!");
 }
 
-loadModel();
+async function init() {
+  await setupWebcam();
+  await loadModel();
+  console.log("Webcam + Model Ready");
+}
+
+init();
 
 async function predict() {
-  // Example input (modify to your model's input shape)
-  const input = tf.tensor([[1, 2, 3, 4]]);
-  
-  const prediction = model.predict(input);
+  const img = tf.browser.fromPixels(webcamElement)
+    .resizeNearestNeighbor([224, 224]) // adjust to your model input size
+    .toFloat()
+    .expandDims();
+
+  const prediction = model.predict(img);
   prediction.print();
 }
